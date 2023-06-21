e1d0d717d1ebbc556fa0fba06cc46d6fbd39a6ab
miguelmorob79 <131287463+miguelmorob79@users.noreply.github.com>
2023-06-21 22:40:12 +0200

    objetos

--- index.js
+++ index.js
@@ -43,32 +43,52 @@ let Perro = {
 let Concierto = {
     grupos: [grupo1,grupo2,grupo3]
 };
-let grupos = Concierto.grupos;
+let grupos = grupos;
 
 // 8.- Dado un **objeto** de nombre **Led** obtén el valor de las propiedades **rojo, verde y azul** guardándolo en la variable **array RGB[Rojo, Verde,Azul]
+Led={
+    rojo:[],
+    verde:[],
+    azul:[]
+}
+RGB=[Led.rojo,Led.verde,Led.azul]
     
-let RGB =
+
 
 // 9.- Dado un **objeto** de nombre **Portatil** modifica el valor de la propiedad **modelo** por el valor **P345**
 
 Portatil. modelo = P345
 
 // 10.- Dado un **objeto** de nombre **Concierto** añade el valor **Guns N' Roses** a la propiedad **cartelera**
-let Concierto{
-    cartelera:('Guns N'Roses')
-}
+
+Concierto.cartelera.push=("Guns N'Roses")
+
+
 // 11.- Dado un **objeto** de nombre **Concierto** modifica el valor de la propiedad **fecha** por el valor **new Date() (fecha de hoy)**
 
-// 12.- Dado un **objeto** de nombre **Impresora** modifica el valor de la propiedad **imprimiendo** por el valor **objeto con propiedades: nombreArchivo, copias, numPaginas**
+Concierto.fecha= new Date (21/6/2023)
 
+// 12.- Dado un **objeto** de nombre **Impresora** modifica el valor de la propiedad **imprimiendo** por el valor **objeto con propiedades: nombreArchivo, copias, numPaginas**
+Impresora.imprimiendo = (nombreArchivo,copias,numPaginas)
 
 // 13.- Crea un **objeto** de nombre **Noticia** que tenga las propiedades: **titular, cuerpo**
-
+const Noticia ={
+    titular:[],
+    cuerpo:[],
+}
 // 14.- Crea un **objeto** de nombre **Persona** que tenga las propiedades: **nombre, apellidos, edad**
 
 // 15.- Crea un **objeto** de nombre **Avion** que tenga las propiedades: **numPasajeros, función despegar (imprime por consola 'despegando'), función volar (imprime por consola llegando al destino), función aterrizar (imprime por consola 'aterrizando'**
+let Avion = {
+    numPasajeros: [],
+    despegar :()=>{ console.log('despegando')},
+    volar: ()=>{ (llegando)}
+}
+
 
 // 16.- Crea un **objeto** de nombre **Paquete** que tenga las propiedades: **array contenido con todos los objetos que contenga el paquete**
+const Paquete ={
+   ["cosa1","cosa2","cosa3"] }
 
 // 17.- Crea un **objeto** de nombre **Pais** que tenga las propiedades: **numHabitantes, continente, gentilicio**
 
@@ -77,7 +97,10 @@ let Concierto{
 
 // 19.- Dado un **objeto** de nombre **Grupo** obtén el valor de la propiedad **array integrantes** guardándolo en la variable **integrantes**
 
+let integrantes=Grupo.integrantes
+
 // 20.- Dado un **objeto** de nombre **Impresora** obtén el valor de la propiedad **objeto tinta{rojo, verde, azul}** guardándolo en la variable **nivelesTinta**
+
 
 // 21.- Dado un **objeto** de nombre **Pantalla** obtén el valor de la propiedad **array bidimensional pixeles** guardándolo en la variable **pixeles**
 
@@ -86,8 +109,16 @@ let Concierto{
 
 // 23.- Dado un **objeto** de nombre **Grupo** modifica el valor de la propiedad **numIntegrantes** por el valor **5**
 
+grupos.numIntegrantes ='5'
+
 // 24.- Dado un **objeto** de nombre **Pantalla** modifica el valor de la propiedad **dimensiones** por el valor **1920x1080**
+
+Pantalla.dimensiones='1920*1080'
 
 // 25.- Dado un **objeto** de nombre **Led** modifica el valor de la propiedad **encendido** por el valor **false si vale true y true si vale false**
 
-// 26.- Dado un **objeto** de nombre **Movil** modifica el valor de la propiedad **temperatura** por el valor **20º**
+let.encendido=true ? false:true;
+
+// 26.- Dado un ** false:trueobjeto** de nombre **Movil** modifica el valor de la propiedad **temperatura** por el valor **20º**
+
+Movil.temperatura='20'
